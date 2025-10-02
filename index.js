b32723bfad4b174f376d560fa4dabe6fdb784cc9
Cristian Ramirez <ccrhdbj@outlook.com>
2025-10-02 07:51:20 -0500

    feat: implementar servidor Express con rutas básicas

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 5555;
+const routerApi = require('./routes');
+
+//middleware recibir json
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.redirect('/new-route');
+});
+
+app.get('/new-route', (req, res) => {
+  res.send('Hello this is a new endpoint');
+});
+
+routerApi(app)
+
+app.listen(port, () => {
+  console.log('mi port' + port);
+});
