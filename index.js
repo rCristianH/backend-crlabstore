2271f56e7f33335d56f0f160cdbf5baffdab218f
Cristian Ramirez <ccrhdbj@outlook.com>
2025-10-02 07:51:36 -0500

    fix: corregir mensaje de respuesta en la nueva ruta

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/new-route', (req, res) => {
-  res.send('Hello this is a new endpoint');
+  res.send('this is a new endpoint');
 });
 
 routerApi(app)
