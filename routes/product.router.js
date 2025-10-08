f4d4b22401114869ce4af666c99585de4a9c3a57
Cristian Ramirez <ccrhdbj@outlook.com>
2025-10-08 13:10:44 -0500

    feat: implementar servicio de productos con métodos CRUD y datos iniciales

--- routes/product.router.js
+++ routes/product.router.js
@@ -1,281 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const ProductsService = require('./../services/product.service');
+const service = new ProductsService();
 
-const ideapads = [
-  {
-    id: 'IS314AR3',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 3 14 AMD R3',
-    ref: 'SLIM3',
-    price: 1550000,
-    cpu: 'AMD Ryzen™ 3 7320U',
-    logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
-    logoCpu: 'https://i.imgur.com/bSdcWWd.jpeg',
-    ram: '8 GB LPDDR5-5500MHz',
-    storage: '512 GB SSD M.2 2242 PCIe TLC',
-    images: [
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
-      'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
-    ],
-  },
-  {
-    id: 'IS314AR5',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 3 14 AMD R5',
-    ref: 'SLIM3',
-    price: 2050000,
-    cpu: 'AMD Ryzen™ 5 7520U',
-    logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
-    logoCpu: 'https://i.imgur.com/AkrWerl.jpeg',
-    ram: '16 GB LPDDR5-5500MHz',
-    storage: '512 GB SSD M.2 2242 PCIe TLC',
-    images: [
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
-      'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
-    ],
-  },
-  {
-    id: 'IS315AR3',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 3 15 AMD R3',
-    ref: 'SLIM3',
-    price: 1600000,
-    cpu: 'AMD Ryzen™ 3 7320U',
-    logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
-    logoCpu: 'https://i.imgur.com/bSdcWWd.jpeg',
-    ram: '8 GB LPDDR5-5500MHz',
-    storage: '512 GB SSD M.2 2242 PCIe TLC',
-    images: [
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
-      'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
-    ],
-  },
-  {
-    id: 'IS315AR5',
-    selected: true,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 3 15 AMD R5',
-    ref: 'SLIM3',
-    clients: ['02CP'],
-    price: 1800000,
-    cpu: 'AMD Ryzen™ 5 7520U',
-    logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
-    logoCpu: 'https://i.imgur.com/AkrWerl.jpeg',
-    ram: '8 GB LPDDR5-5500MHz',
-    storage: '512 GB SSD M.2 2242 PCIe TLC',
-    images: [
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
-      'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
-    ],
-  },
-  {
-    id: 'IS315AR7',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 3 15 AMD R7',
-    ref: 'SLIM3',
-    price: 2200000,
-    cpu: 'AMD Ryzen™ 7 7730U',
-    logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
-    logoCpu: 'https://i.imgur.com/1jffKoA.jpeg',
-    ram: '16 GB DDR4-3200MHz',
-    storage: '512 GB SSD M.2 2242 PCIe QLC',
-    images: [
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
-      'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
-    ],
-  },
-  {
-    id: 'IS315IC3',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 3 15 INT 3',
-    ref: 'SLIM3',
-    price: 1700000,
-    cpu: 'Intel® Core™ i3-1305U',
-    logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
-    logoCpu: 'https://i.imgur.com/fWpEXUk.jpeg',
-    ram: '8 GB LPDDR5-4800MHz',
-    storage: '',
-    images: [
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
-      'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
-    ],
-  },
-  {
-    id: 'IS315IC5',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 3 15 INT 5',
-    ref: 'SLIM3',
-    price: 2400000,
-    cpu: 'Intel® Core™ i5-12450H',
-    logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
-    logoCpu: 'https://i.imgur.com/S2WUFqN.jpeg',
-    ram: '16 GB LPDDR5-4800MHz',
-    storage: '512 GB SSD M.2 2242 PCIe Gen4 TLC',
-    images: [
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
-      'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
-    ],
-  },
-  {
-    id: 'IS316IC5',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 5 16 INT 5',
-    ref: 'SLIM5',
-    price: 2100000,
-    cpu: 'Intel® Core™ i5-12450H',
-    logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
-    logoCpu: 'https://i.imgur.com/S2WUFqN.jpeg',
-    ram: '8 GB LPDDR5-4800MHz',
-    storage: '512 GB SSD M.2 2242 PCIe Gen4 TLC',
-    images: [
-      'https://p1-ofp.static.pub//medias/25730676944_CG_MEALNP_202304120105021694841498625.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/05/2fe836g93vaehq5bqefyr5n3uryldg141098.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/05/thwwou371n7molooemeylcohjrhk0a371300.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/05/6jslp0z3l76yrzsp9nml0qba2xghfd561804.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/05/sknpr6ez6rq65q3kpz97zycpv8om7u149211.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/05/jv4n78v76me0uqne2fsqckijhsdqr5839627.png',
-    ],
-  },
-  {
-    id: 'IS316IC51TB',
-    selected: false,
-    available: true,
-    family: 'Lenovo Ideapad',
-    shortSerie: 'Ideapad',
-    title: 'Slim 5 16 INT 5',
-    ref: 'SLIM5',
-    price: 2300000,
-    cpu: 'Intel® Core™ i5-12450H',
-    logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
-    logoCpu: 'https://i.imgur.com/S2WUFqN.jpeg',
-    ram: '16 GB LPDDR5-4800MHz',
-    images: [
-      'https://p1-ofp.static.pub//medias/25730676944_CG_MEALNP_202304120105021694841498625.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/05/2fe836g93vaehq5bqefyr5n3uryldg141098.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/05/thwwou371n7molooemeylcohjrhk0a371300.png',
-      'https://p1-ofp.static.pub/fes/cms/2022/12/05/6jslp0z3l76yrzsp9nml0qba2xghfd561804.png',
-      'https://p2-ofp.static.pub/fes/cms/2022/12/05/sknpr6ez6rq65q3kpz97zycpv8om7u149211.png',
-      'https://p4-ofp.static.pub/fes/cms/2022/12/05/jv4n78v76me0uqne2fsqckijhsdqr5839627.png',
-    ],
-  },
-];
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const { size } = req.query;
-  const limit = size || 5;
-  res.json(ideapads.slice(0, limit));
+  const limit = size ? Number(size) : service.find().length;
+  const products = await service.find();
+  res.json(products.slice(0, limit));
 });
 
-router.get('/itemsquery', (req, res) => {
-  const { limit, offset } = req.query;
-
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('no query param');
-  }
-});
-
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = ideapads.find((item) => item.id === id);
+  const product = await service.findOne(id);
   if (!product) {
     return res.status(404).json({ message: 'Product not found' });
   }
   res.status(200).json(product);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body,
-  });
+  await service.create(body);
+  res.status(201).json(body);
 });
 
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    res.json(await service.update(id, body));
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  res.json({
-    message: 'delete sucesfully',
-  });
+  res.json(await service.delete(id));
 });
 
 module.exports = router;
---
+++ services/product.service.js
@@ -0,0 +1,299 @@
+class ProductsService {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const ideapads = [
+      {
+        id: 'IS314AR3',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 3 14 AMD R3',
+        ref: 'SLIM3',
+        price: 1550000,
+        cpu: 'AMD Ryzen™ 3 7320U',
+        logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
+        logoCpu: 'https://i.imgur.com/bSdcWWd.jpeg',
+        ram: '8 GB LPDDR5-5500MHz',
+        storage: '512 GB SSD M.2 2242 PCIe TLC',
+        images: [
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
+          'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
+        ],
+      },
+      {
+        id: 'IS314AR5',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 3 14 AMD R5',
+        ref: 'SLIM3',
+        price: 2050000,
+        cpu: 'AMD Ryzen™ 5 7520U',
+        logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
+        logoCpu: 'https://i.imgur.com/AkrWerl.jpeg',
+        ram: '16 GB LPDDR5-5500MHz',
+        storage: '512 GB SSD M.2 2242 PCIe TLC',
+        images: [
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
+          'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
+        ],
+      },
+      {
+        id: 'IS315AR3',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 3 15 AMD R3',
+        ref: 'SLIM3',
+        price: 1600000,
+        cpu: 'AMD Ryzen™ 3 7320U',
+        logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
+        logoCpu: 'https://i.imgur.com/bSdcWWd.jpeg',
+        ram: '8 GB LPDDR5-5500MHz',
+        storage: '512 GB SSD M.2 2242 PCIe TLC',
+        images: [
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
+          'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
+        ],
+      },
+      {
+        id: 'IS315AR5',
+        selected: true,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 3 15 AMD R5',
+        ref: 'SLIM3',
+        clients: ['02CP'],
+        price: 1800000,
+        cpu: 'AMD Ryzen™ 5 7520U',
+        logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
+        logoCpu: 'https://i.imgur.com/AkrWerl.jpeg',
+        ram: '8 GB LPDDR5-5500MHz',
+        storage: '512 GB SSD M.2 2242 PCIe TLC',
+        images: [
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
+          'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
+        ],
+      },
+      {
+        id: 'IS315AR7',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 3 15 AMD R7',
+        ref: 'SLIM3',
+        price: 2200000,
+        cpu: 'AMD Ryzen™ 7 7730U',
+        logoBrandCPU: 'https://i.imgur.com/w0mkA3c.png',
+        logoCpu: 'https://i.imgur.com/1jffKoA.jpeg',
+        ram: '16 GB DDR4-3200MHz',
+        storage: '512 GB SSD M.2 2242 PCIe QLC',
+        images: [
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
+          'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
+        ],
+      },
+      {
+        id: 'IS315IC3',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 3 15 INT 3',
+        ref: 'SLIM3',
+        price: 1700000,
+        cpu: 'Intel® Core™ i3-1305U',
+        logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
+        logoCpu: 'https://i.imgur.com/fWpEXUk.jpeg',
+        ram: '8 GB LPDDR5-4800MHz',
+        storage: '',
+        images: [
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
+          'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
+        ],
+      },
+      {
+        id: 'IS315IC5',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 3 15 INT 5',
+        ref: 'SLIM3',
+        price: 2400000,
+        cpu: 'Intel® Core™ i5-12450H',
+        logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
+        logoCpu: 'https://i.imgur.com/S2WUFqN.jpeg',
+        ram: '16 GB LPDDR5-4800MHz',
+        storage: '512 GB SSD M.2 2242 PCIe Gen4 TLC',
+        images: [
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/wl57o6uv41lwkq1zah6pkt22ihvrp8950736.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/eluleo8r9wdf4fp609a02wcs6elvvs288133.png',
+          'https://p3-ofp.static.pub/fes/cms/2022/12/09/cexb4tv7i0htpsnfs1ebckpfeu9a02898002.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/m3h7niiqfx533jd4ifivoifk8hwbqa695193.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/niby8swvs97v5d8j4gmkmxvjvuhd1i806429.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/09/o9u9t11vzcc8xsnvc9tuk7qo0swvvs045529.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/09/3t4ae2dodx8qq5sjdxeorl0p8q87ag986732.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/09/tux4q0ovfxyxep9yu6xmp4ik0oglfz933016.png',
+        ],
+      },
+      {
+        id: 'IS316IC5',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 5 16 INT 5',
+        ref: 'SLIM5',
+        price: 2100000,
+        cpu: 'Intel® Core™ i5-12450H',
+        logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
+        logoCpu: 'https://i.imgur.com/S2WUFqN.jpeg',
+        ram: '8 GB LPDDR5-4800MHz',
+        storage: '512 GB SSD M.2 2242 PCIe Gen4 TLC',
+        images: [
+          'https://p1-ofp.static.pub//medias/25730676944_CG_MEALNP_202304120105021694841498625.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/05/2fe836g93vaehq5bqefyr5n3uryldg141098.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/05/thwwou371n7molooemeylcohjrhk0a371300.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/05/6jslp0z3l76yrzsp9nml0qba2xghfd561804.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/05/sknpr6ez6rq65q3kpz97zycpv8om7u149211.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/05/jv4n78v76me0uqne2fsqckijhsdqr5839627.png',
+        ],
+      },
+      {
+        id: 'IS316IC51TB',
+        selected: false,
+        available: true,
+        family: 'Lenovo Ideapad',
+        shortSerie: 'Ideapad',
+        title: 'Slim 5 16 INT 5',
+        ref: 'SLIM5',
+        price: 2300000,
+        cpu: 'Intel® Core™ i5-12450H',
+        logoBrandCPU: 'https://i.imgur.com/8oHRHNY.png',
+        logoCpu: 'https://i.imgur.com/S2WUFqN.jpeg',
+        ram: '16 GB LPDDR5-4800MHz',
+        images: [
+          'https://p1-ofp.static.pub//medias/25730676944_CG_MEALNP_202304120105021694841498625.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/05/2fe836g93vaehq5bqefyr5n3uryldg141098.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/05/thwwou371n7molooemeylcohjrhk0a371300.png',
+          'https://p1-ofp.static.pub/fes/cms/2022/12/05/6jslp0z3l76yrzsp9nml0qba2xghfd561804.png',
+          'https://p2-ofp.static.pub/fes/cms/2022/12/05/sknpr6ez6rq65q3kpz97zycpv8om7u149211.png',
+          'https://p4-ofp.static.pub/fes/cms/2022/12/05/jv4n78v76me0uqne2fsqckijhsdqr5839627.png',
+        ],
+      },
+    ];
+    this.products = ideapads;
+  }
+
+  create(body) {
+    this.products.push(body);
+    return body;
+  }
+
+  find() {
+    return new Promise((resolve, reject) => {
+      return this.products;
+    });
+  }
+
+  findOne(id) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    if ('id' in changes) {
+      throw new Error('Cannot change the id property');
+    }
+    const product = this.products[index];
+    for (const key in changes) {
+      if (!(key in product)) {
+        throw new Error(`Property "${key}" does not exist`);
+      }
+      product[key] = changes[key];
+    }
+    return product;
+  }
+
+  delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    const deletedProduct = this.products[index];
+    this.products.splice(index, 1);
+    return {
+      message: `Product with id ${id} was deleted successfully`,
+      deletedProduct,
+    };
+  }
+
+  deleteMany(ids) {
+    if (!Array.isArray(ids)) {
+      throw new Error('ids must be an array');
+    }
+    const deleted = [];
+    ids.forEach((id) => {
+      const index = this.products.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        deleted.push(this.products[index]);
+        this.products.splice(index, 1);
+      }
+    });
+    return {
+      message: `Deleted ${deleted.length} products`,
+      deletedProducts: deleted,
+    };
+  }
+}
+
+module.exports = ProductsService;
